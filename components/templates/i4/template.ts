3487e0f26e65e7ac863aa460c67e3bc8742fdadc
Le Van Quy <vannn.quy@gmail.com>
2025-07-28 22:19:35 +0700

    up

--- components/templates/i4/template.ts
+++ components/templates/i4/template.ts
@@ -23,18 +23,18 @@ export const i4Template = (data: {
     const positions = [
       // [top, left] cho mỗi cột
       [2.7, 11.9],   // cột 1
-      [20, 29.3],  // cột 2  
-      [37.3, 46.6]   // cột 3
+      [22, 31.3],  // cột 2  
+      [41.3, 50.6]   // cột 3
     ];
     
-    const baseTop = [3.9, 9.4, 14.9, 20.4, 25.9, 31.4, 36.9, 42.4]; // top positions cho mỗi hàng
+    const baseTop = [3.9, 10.4, 16.9, 23.4, 29.9, 36.4, 42.9, 49.4]; // top positions cho mỗi hàng
     
     const currentTop = baseTop[row];
     const currentLeft = positions[col][0];
     const originLeft = positions[col][1];
     
          return `
- <div style="position:absolute;top:${currentTop}rem;left:${currentLeft}rem;width:15.6rem;">
+ <div style="position:absolute;top:${currentTop}rem;left:${currentLeft}rem;width:18rem;">
    <p style="white-space:normal;overflow:hidden;word-wrap:break-word;line-height:1.0;" class="ft14"><b>${data.product_name}<br/></b>${data.product_info}</p>
    <p style="white-space:nowrap;overflow:hidden;text-overflow:ellipsis;margin-top:1.25rem;line-height:1.0;" class="ft15"><b>Prodávající: ${data.pt_brand}<br/>${data.pt_product_code}: ${data.product_code} <span style="float:right;">${data.pt_origin_country}: ${data.country_name}</span></b></p>
  </div>`;
@@ -55,17 +55,17 @@ export const i4Template = (data: {
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <style type="text/css">
 <!--
-	p {margin: 0; padding: 0;}	.ft10{font-size:6px;font-family:Times;color:#000000;}
-	.ft11{font-size:6px;font-family:Times;color:#000000;}
-	.ft12{font-size:8px;font-family:Times;color:#000000;}
-	.ft13{font-size:6px;line-height:10px;font-family:Times;color:#000000;}
-	.ft14{font-size:6px;line-height:10px;font-family:Times;color:#000000;}
-	.ft15{font-size:8px;line-height:11px;font-family:Times;color:#000000;}
+	p {margin: 0; padding: 0;}	.ft10{font-size:8px;font-family:Times;color:#000000;}
+	.ft11{font-size:8px;font-family:Times;color:#000000;}
+	.ft12{font-size:10px;font-family:Times;color:#000000;}
+	.ft13{font-size:8px;line-height:12px;font-family:Times;color:#000000;}
+	.ft14{font-size:8px;line-height:12px;font-family:Times;color:#000000;}
+	.ft15{font-size:10px;line-height:13px;font-family:Times;color:#000000;}
 -->
 </style>
 </head>
 <body vlink="blue" link="blue">
-<div id="page1-div" style="position:relative;width:55.8rem;height:78.9rem;">
+<div id="page1-div" style="position:relative;width:65rem;height:85rem;">
 ${allLabels}
 </div>
 </body>
